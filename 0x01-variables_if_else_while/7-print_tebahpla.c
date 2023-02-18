fa26722d5ea40f2894f1f7b1a9b2614465f27d55
Faith udoh <122934763+Herselfy@users.noreply.github.com>
2023-02-18 05:39:25 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Hex values
- * Return: Always 0 (Success)
+ *main - entry point
+ *reversing the order of the alphabet
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int n;
-char c;
-for (n = 0; n <= 9; n++)
+char rv_alphabet;
+for (rv_alphabet = 'z'; rv_alphabet >= 'a'; rv_alphabet--)
 {
-putchar((n % 10) + '0');
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-putchar(c);
+putchar(rv_alphabet);
 }
 putchar('\n');
 return (0);
