45c4b70623478212a95277b633eb4de6bf0832dc
herselfy <fsylvester17@gmail.com>
2023-02-22 09:38:28 -0800

    digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * print_last_digit - entry point
+ * Return: Always 0 (Success)
+ * @i: return last value
+ */
+
+int print_last_digit(int i)
+{
+int val;
+val = i % 10;
+if (val < 0)
+val *= -1;
+
+_putchar(val + '0');
+
+return (val);
+}
