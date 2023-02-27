d209d1ffc9984e724b94ca51aede6df00bcfee92
herselfy <fsylvester17@gmail.com>
2023-02-27 12:29:16 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * puts2 - print the values in even notation
  *
- * Return: Always 0.
+ * @str: print in even notation
  */
-int main(void)
-{
-    char *str;
 
-    str = "0123456789";
-    puts2(str);
-    return (0);
+void puts2(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i]; i += 1)
+		if (str[i] != '\0')
+			count += 1;
+		else
+			break;
+	for (i = 0; i < count; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
 }
