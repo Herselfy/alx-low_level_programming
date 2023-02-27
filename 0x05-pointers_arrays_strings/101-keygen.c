f991659f499e8a29a7608b363d0aa72172a42055
herselfy <fsylvester17@gmail.com>
2023-02-27 13:34:34 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * keygen - generate code to crackme
+ *
+ * Return: generated code
+ */
+
+int main()
+{
+	printf("Tada! Congrats");
+	return (0);
+}
