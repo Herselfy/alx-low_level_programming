f69cde7b7cb34b0061fb413f2017b8e727e04e88
herselfy <fsylvester17@gmail.com>
2023-04-04 15:01:36 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at a given index
+ * @head: get the nodes
+ * @index: get the index to be deleted
+ * Return: nodes not deleted
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *cp = *head;
+	unsigned int i = 0;
+
+	if (cp == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(cp);
+		return (1);
+	}
+
+	while (i < (index - 1))
+	{
+		if (cp->next == NULL)
+			return (-1);
+
+		cp = cp->next;
+		i++;
+	}
+
+	tmp = cp->next;
+	cp->next = tmp->next;
+	free(tmp);
+
+	return (1);
